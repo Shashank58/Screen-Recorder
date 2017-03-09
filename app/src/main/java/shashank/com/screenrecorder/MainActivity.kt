6307ff1c317d0a26475a58220fafed8791297678
shashankm <shashank@smallcase.com>
2017-03-09 15:15:41 +0530

    Start and stop screen recording

---
+++ app/src/main/java/shashank/com/screenrecorder/MainActivity.kt
@@ -0,0 +1,69 @@
+package shashank.com.screenrecorder
+
+import android.Manifest
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.media.MediaRecorder
+import android.media.projection.MediaProjectionManager
+import android.os.Bundle
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
+import android.support.v7.app.AppCompatActivity
+import android.util.DisplayMetrics
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    private val REQUEST_PERMISSION = 2
+    private var screenRecordHelper: ScreenRecordHelper? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val displayMetrics: DisplayMetrics = DisplayMetrics()
+        windowManager.defaultDisplay.getMetrics(displayMetrics)
+
+        val mediaRecorder: MediaRecorder = MediaRecorder()
+        val projectionManager = getSystemService(Context.MEDIA_PROJECTION_SERVICE) as MediaProjectionManager
+        val screenDensity = displayMetrics.densityDpi
+        screenRecordHelper = ScreenRecordHelper(projectionManager, mediaRecorder, this, screenDensity)
+
+        start_recording.setOnClickListener(this)
+        stop_recording.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        if (v != null) when (v.id) {
+            R.id.start_recording -> {
+                if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) !=
+                        PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest
+                            .permission.RECORD_AUDIO), REQUEST_PERMISSION)
+                    return
+                }
+                screenRecordHelper?.initRecording()
+            }
+
+            R.id.stop_recording -> screenRecordHelper?.stopRecording()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode != screenRecordHelper?.REQUEST_CODE || resultCode != Activity.RESULT_OK) {
+            return
+        }
+
+        screenRecordHelper?.registerMediaProjection(resultCode, data)
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        if (requestCode == REQUEST_PERMISSION) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                screenRecordHelper?.initRecording()
+            }
+        }
+    }
+}
---
+++ app/src/main/java/shashank/com/screenrecorder/ScreenRecordHelper.kt
@@ -0,0 +1,110 @@
+package shashank.com.screenrecorder
+
+import android.content.Intent
+import android.hardware.display.DisplayManager
+import android.hardware.display.VirtualDisplay
+import android.media.MediaRecorder
+import android.media.projection.MediaProjection
+import android.media.projection.MediaProjectionManager
+import android.os.Environment
+import android.util.SparseIntArray
+import java.io.IOException
+
+/**
+ * Created by shashankm on 02/03/17.
+ */
+class ScreenRecordHelper(val projectionManager: MediaProjectionManager, val mediaRecorder: MediaRecorder, val
+            activity: MainActivity, val screenDensity: Int) {
+    val REQUEST_CODE = 1
+
+    private val ORIENTATION = SparseIntArray()
+    private val DISPLAY_WIDTH = 720
+    private val DISPLAY_HEIGHT = 1280
+
+    private var mediaProjection: MediaProjection? = null
+    private var virtualDisplay: VirtualDisplay? = null
+    private val mediaProjectionCallback: MediaProjectionCallback = MediaProjectionCallback(this)
+
+    fun initRecording() {
+        try {
+            mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC)
+            mediaRecorder.setVideoSource(MediaRecorder.VideoSource.SURFACE)
+            mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP)
+            mediaRecorder.setOutputFile(Environment.getExternalStorageDirectory().path + "/video.mp4")
+            mediaRecorder.setVideoSize(DISPLAY_WIDTH, DISPLAY_HEIGHT)
+            mediaRecorder.setVideoEncoder(MediaRecorder.VideoEncoder.H264)
+            mediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB)
+            mediaRecorder.setVideoEncodingBitRate(512 * 1000)
+            mediaRecorder.setVideoFrameRate(30)
+            val rotation = activity.windowManager.defaultDisplay.rotation
+            mediaRecorder.setOrientationHint(ORIENTATION.get(rotation + 90))
+            mediaRecorder.prepare()
+            startRecording()
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+    }
+
+    private fun startRecording() {
+        if (mediaProjection == null) {
+            activity.startActivityForResult(projectionManager.createScreenCaptureIntent(), REQUEST_CODE)
+            return
+        }
+
+        virtualDisplay = createVirtualDisplay()
+        mediaRecorder.start()
+    }
+
+    private fun createVirtualDisplay(): VirtualDisplay? {
+        return mediaProjection?.createVirtualDisplay("Main Activity", DISPLAY_WIDTH, DISPLAY_HEIGHT, screenDensity,
+                DisplayManager.VIRTUAL_DISPLAY_FLAG_AUTO_MIRROR, mediaRecorder.surface, null, null)
+    }
+
+    fun stopRecording() {
+        mediaRecorder.stop()
+        mediaRecorder.reset()
+
+        if (virtualDisplay == null) {
+            return
+        }
+
+        virtualDisplay?.release()
+        destroyMediaProjection()
+    }
+
+    fun registerMediaProjection(resultCode: Int, data: Intent?) {
+        mediaProjection = projectionManager.getMediaProjection(resultCode, data)
+        mediaProjection?.registerCallback(mediaProjectionCallback, null)
+        startRecording()
+    }
+
+    private fun destroyMediaProjection() {
+        if (mediaProjection != null) {
+            mediaProjection?.unregisterCallback(mediaProjectionCallback)
+            mediaProjection?.stop()
+            mediaProjection = null
+        }
+    }
+
+    private class MediaProjectionCallback(val screenRecordHelper: ScreenRecordHelper) : MediaProjection.Callback() {
+
+
+        override fun onStop() {
+            super.onStop()
+            screenRecordHelper.mediaRecorder.stop()
+            screenRecordHelper.mediaRecorder.reset()
+
+            screenRecordHelper.mediaProjection = null
+            screenRecordHelper.stopScreenSharing()
+        }
+    }
+
+    private fun stopScreenSharing() {
+        if (virtualDisplay == null) {
+            return
+        }
+
+        virtualDisplay?.release()
+        destroyMediaProjection()
+    }
+}
